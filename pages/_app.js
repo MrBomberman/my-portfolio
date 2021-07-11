a6fd16c3ae7a85b6314859fd2bb5849976ef22ba
Kirill <mrbomberman99@gmail.com>
2021-07-11 19:28:52 +0300

    добавил файл _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/main.css'
+import NextNprogress from 'nextjs-progressbar';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) { // Component - та страница, которую мы отображаем в зависимости от url адреса
+  return (
+          <>
+              <NextNprogress
+              color="yellow"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+              showOnShallow={true}
+              />
+        <Component {...pageProps} />
+
+          </>
+  )
 }
 
 export default MyApp
