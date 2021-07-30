67041817d5f59f60e1cc63dff66d85d9bc595e4b
Kirill <mrbomberman99@gmail.com>
2021-07-30 20:51:00 +0300

    импортирую компоненты на нужные страницы

--- pages/js30/clock/index.js
+++ pages/js30/clock/index.js
@@ -1,57 +1,11 @@
-import React, { Component, createRef } from 'react';
-import classes from './clock.module.css';
+import Clock from '../../../components/js30/clock';
 
-class Clock extends Component{
-    constructor(props){
-        super(props);
-        this.clockSec = React.createRef();
-        this.clockMin = React.createRef();
-        this.clockHour = React.createRef();
-    }
-
-    setDate() {
-        const now = new Date();
-        const seconds = now.getSeconds();
-        const minutes = now.getMinutes();
-        const hours = now.getHours();
-        const secondsDegrees = ((seconds / 60) * 360) + 90; // делим кол-во секунд на 60 и умножаем на 360 градусов, чтобы получить нужный градус стрелки
-        const minutesDegrees = (360/60) * minutes + 90; // то же самое для минут, только стрелка меняет реже свое положение
-        const hoursDegrees = (360/12)*hours + 90; // раз в час стрелка меняет свое положение на 30 градусов
-        this.clockSec.current.style.transform = `rotate(${secondsDegrees}deg)`; // задаем стиль для секундной стрелки, на какое кол-во градусов ей нужно поворачиваться каждую секунду
-        this.clockMin.current.style.transform = `rotate(${minutesDegrees}deg)`;
-        this.clockHour.current.style.transform = `rotate(${hoursDegrees}deg)`;
-    }
-    componentDidMount(){
-        this.interval = setInterval(() => this.setDate(), 1000);
-    }
-    
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-      
-
-    render(){
-        // setInterval(this.setDate, 1000)
-        
-
-        
+function ClockPage(){
     return (
-        <> 
-        <div className={classes.container}>
-            <div className={classes.clock}>
-                <div className={classes.clockFace}>
-                <div className={classes.hand} ref={this.clockHour}>
-                </div>
-                <div className={classes.hand} ref={this.clockMin}>
-                </div>
-                <div className={classes.hand} ref={this.clockSec}>
-                </div>
-                </div>
-            </div>
-        </div>
+        <>
+        <Clock/>
         </>
     )
-    }
 }
 
-export default Clock;
+export default ClockPage;
--- pages/js30/index.js
+++ pages/js30/index.js
@@ -1,19 +1,10 @@
-import Link from "next/link";
-import classes from './js30.module.css';
+import Js30 from "../../components/js30";
 
-function Js30(){
+function Js30Page(){
     return (
         <>
-        <div className={classes.container}>
-            <h1 className={classes.title}>You can see some interesting projects from coding challenge (JS_30)</h1>
-            <div className={classes.content}>
-                <div className={classes.project}>
-                    <Link href='/js30/clock'><p className={classes.projectBtn}>Go to see clock</p></Link>
-                </div>
-            </div>
-            <Link href='/'><p>Go to the main page</p></Link>
-        </div>
+        <Js30/>
         </>
     )
 }
-export default Js30;
+export default Js30Page;
