f5b89f9048422de23f02e77382c3ba9e252ca6c9
Kirill <mrbomberman99@gmail.com>
2022-03-17 12:05:00 +0300

    added appearance for icon in timeline section - it appears on page while user is scrolling

--- components/main-page/timelineCard/index.js
+++ components/main-page/timelineCard/index.js
@@ -1,10 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
 import classes from '../main-page.module.scss'
 
 export default function TimelineCard(){
+
+    const timelineCard = useRef();
+    const iconTimelineRef = useRef();
+
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset);
+
+        const slideAppearing = ((window.scrollY + 50) + window.innerHeight) - (timelineCard.current.clientHeight / 2); 
+        const slideBottom = timelineCard.current.offsetTop + timelineCard.current.clientHeight;
+        const needToShow = slideAppearing > timelineCard.current.offsetTop;
+        const isNotScrolledPast = (window.scrollY + 50) < slideBottom;
+
+        if(needToShow && isNotScrolledPast){
+            iconTimelineRef.current.className = `${classes.iconTimeline} ${classes.iconTimelineAnimation}` 
+        } else {
+            iconTimelineRef.current.className = `${classes.iconTimeline}` 
+        }
+
+        // clean up code
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [offset]);
+
+
     return (
 
-        <div className={classes.timelineSectionCard}>
-        <span className={classes.iconTimeline}>
+        <div ref={timelineCard} className={classes.timelineSectionCard}>
+        <span ref={iconTimelineRef} className={classes.iconTimeline}>
           <svg></svg>
         </span>
             <div className={classes.cardContent}>
