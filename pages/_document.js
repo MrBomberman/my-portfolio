3e6876452d08a71bd58a10b576e411ef89c94a67
Kirill <mrbomberman99@gmail.com>
2022-03-18 11:07:57 +0300

    made text more readable for info paragraph

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/brands.css" rel="stylesheet"/>
         <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.css' rel='stylesheet'/> 
-        <script defer src="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js"></script>
+        {/* <link href="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js"></link> */}
         </Head>
         <body>
           <Main />
